6967836136c57db7a4dca40202418a910b89720a
Kirill Sannikov <kir.sannikow@mail.ru>
2019-08-01 16:10:16 +0300

    Change main.php

--- main.php
+++ main.php
@@ -4,51 +4,21 @@ namespace App;
 
 require_once 'vendor/autoload.php';
 
-/**
- * Регистрационный номер животного
- *
- * @var integer
- */
-$registrationNumber = 1;
+$farm = new Farm();
 
-// Добавление 20 куриц
-for ($i = 0; $i < 20 ; $i++) {
-    $chicken = new Chicken($registrationNumber);
-    // Сбор яиц
-    $chicken->collectingEgges();
-    /**
-     * Массив с объектами Курица
-     */
-    $chickens[] = $chicken;
+// Добавление куриц
+$chickens = $farm->addingAnimals(20, 0, 1);
+// Сбор яиц у куриц
+$chickens = $farm->collectingProducts($chickens);
+// Подсчет общего количества яиц
+$numberEggs = $farm->countingProducts($chickens);
 
-    $registrationNumber++;
-}
-
-//Добавление 10 коров
-for ($i = 0; $i < 10 ; $i++) {
-    $cow = new Cow($registrationNumber);
-    // Сбор молока
-    $cow->collectingMilk();
-    /**
-     * Массив с объектами Корова
-     */
-    $cows[] = $cow;
-
-    $registrationNumber++;
-}
-/**
- * Количество яиц
- *
- * @var integer
- */
-$numberEggs = array_sum(array_column($chickens, 'eggsQuantity' ));
-/**
- * Количество молока
- *
- * @var integer
- */
-$amountMilk = array_sum(array_column($cows, 'milkQuantity' ));
+// Добавление коров
+$cows = $farm->addingAnimals(10, 8, 12);
+// Сбор молока у коров
+$cows = $farm->collectingProducts($cows);
+// Подсчет общего количества молока
+$amountMilk = $farm->countingProducts($cows);
 
 echo 'Количество яиц: ' . $numberEggs . ' шт' . "\n";
-
 echo 'Количество молока: ' . $amountMilk . ' л';
